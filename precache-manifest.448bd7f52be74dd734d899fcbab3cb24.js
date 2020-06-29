329c5be01971e18caa480d308b8605dd1459976d
henrikolsvik <henrik.hoybakk.olsvik@nav.no>
2020-06-29 13:42:52 +0200

    Updates

---
+++ precache-manifest.448bd7f52be74dd734d899fcbab3cb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeb9465f92d484f947cad0b4f54c8c8",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "92071ce2c3431e598ab4",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f03e285e5cc58aef8a9",
+    "url": "/infoskjerm/static/js/2.26f9fa03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.26f9fa03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92071ce2c3431e598ab4",
+    "url": "/infoskjerm/static/js/main.ecdb68b1.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infoskjerm/static/media/logo.5d5d9eef.svg"
+  }
+]);
